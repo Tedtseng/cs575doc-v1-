e06d1cd83d0dc7c2e18a1bcf13c67274c5ba4144
Tedtseng <tedtseng@bu.edu>
2015-11-24 22:02:19 -0500

    new app.js

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var app = require('express')();
+/*var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http);*/
+var http = require('http');
+var path = require('path');
 
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+/*
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
-});
+});*/
 
-var user_count = 0;
+router.use(express.static(path.resolve(__dirname, '/index.html')));
+var messages = [];
+var sockets = [];
+
+
+
+
+/*var user_count = 0;
 
 //當新的使用者連接進來的時候
 io.on('connection', function(socket){
@@ -41,9 +59,80 @@ io.on('connection', function(socket){
 	});
 
 
+});*/
+
+io.on('connection', function (socket) {
+    messages.forEach(function (data) {
+      socket.emit('message', data);
+    });
+
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+      updateRoster();
+    });
+
+    socket.on('message', function (msg) {
+      var text = String(msg || '');
+
+      if (!text)
+        return;
+
+      socket.get('name', function (err, name) {
+        var data = {
+          name: name,
+          text: text
+        };
+
+        broadcast('message', data);
+        messages.push(data);
+      });
+    });
+
+    socket.on('identify', function (name) {
+      socket.set('name', String(name || 'Anonymous'), function (err) {
+        updateRoster();
+      });
+    });
+  });
+
+function updateRoster() {
+  async.map(
+    sockets,
+    function (socket, callback) {
+      socket.get('name', callback);
+    },
+    function (err, names) {
+      broadcast('roster', names);
+    }
+  );
+}
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
+
+
+
+
+
+
+
+
+//--------------
+//指定port
+/*http.listen(process.env.PORT || 3000, function(){
+   console.log('listening on *:3000');
+});*/
+
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
 
-//指定port
-http.listen(process.env.PORT || 3000, function(){
-	console.log('listening on *:3000');
-});
+	
